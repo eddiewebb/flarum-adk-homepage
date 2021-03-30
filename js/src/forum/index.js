bc52c68adccc61fb02d02a5d5faf7db6e2432af9
Eddie Webb <ollitech@gmail.com>
2021-03-29 20:56:02 -0400

    track conversions

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,18 +1,16 @@
 import AdkHomepage from "./pages/AdkHomepage";
 import { extend } from 'flarum/common/extend';
-import TextEditor from 'flarum/components/TextEditor'
+import SignUpModal from 'flarum/forum/components/SignUpModal';
 
 
 // Register Flarum Blog
 app.initializers.add("adkhomepage", (app) => {
   app.routes.blog = { path: "/adkhome", component: AdkHomepage };
 
-  extend(TextEditor.prototype, 'toolbarItems', function(toolbar) {
-    console.log("Removing image button")
-    console.log(toolbar);
-    console.log(toolbar.items);
-    console.log(toolbar.items.markdown);
-    
+  /* send conversion to goole when somoennsignus up */
+  extend(SignUpModal.prototype, 'onready', function(vnode){
+    console.log("New signup!");
+    gtag_report_conversion();
   });
 
 });
