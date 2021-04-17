a5ca545819e602a968e4ab698247be7608c52f47
Eddie Webb <ollitech@gmail.com>
2021-04-17 10:34:23 -0400

    rtemove eddie specific gtag

--- extend.php
+++ extend.php
@@ -21,34 +21,8 @@ return [
    (new Extend\Frontend('forum'))
     ->js(__DIR__.'/js/dist/forum.js')
     ->css(__DIR__ . '/less/Forum.less')
-    ->route('/adkhome', 'adkhomepage',AdkHomepageController::class)
+    ->route('/adkhome', 'adkhomepage',AdkHomepageController::class),
 
-    ->content(function (Document $document, Request $request) {
-        //var_dump($request);
-
-        $conversionScript = <<<'EOS'
-            <!-- Event snippet for Sign-up conversion page
-            In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
-            <script>
-            function gtag_report_conversion(url) {
-              var callback = function () {
-                if (typeof(url) != 'undefined') {
-                  window.location = url;
-                }
-              };
-              gtag('event', 'conversion', {
-                  'send_to': 'AW-1066146344/bkTgCIa8j_wBEKi0sPwD',
-                  'event_callback': callback
-              });
-              return false;
-            }
-            </script>
-            EOS;
-        $document->head[] = '<script>gtag(\'config\', \'AW-1066146344\');</script>';
-        $document->head[] = $conversionScript;
-    })
-        
-    ,
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__ . '/less/Admin.less'),
